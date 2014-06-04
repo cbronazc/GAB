4e1f55b42df4d15dfd64e24fafbfcb9a11995a2a
Caleb Bron <cbron@azcentral.com>
2014-06-04 10:27:43 -0700

    updated automerger

--- automerger.py
+++ automerger.py
@@ -2,14 +2,14 @@ import datetime
 import config
 import api
 
-def print_message(merging):
+def print_message(merging, pr_id, user, head_ref, base_ref):
   if merging == True:
     message = "Merging: "
   else:
     message = "Not merging: "
   print message + str(pr_id) + " - " + user + " wants to merge " + head_ref + " into " + base_ref
 
-def merge():
+def merge(repo, pr_id):
   data = api.merge_pr(repo, pr_id)
   if "merged" in data and data["merged"]==True:
     print "Merged: " + data['sha']
@@ -23,7 +23,8 @@ def run():
   for repo in config.am_repos:
     data = api.get_prs(repo)
 
-    if data and data['message'] == "Not Found":
+
+    if data and 'message' in data.keys() and data['message'] == "Not Found":
       print "Repo %s not found, make sure it exists"%repo
     else:
       for i in data:
@@ -32,7 +33,7 @@ def run():
         user=i["user"]["login"]
         pr_id = i["number"]
         if base_ref in config.am_ignore_branches:
-          print_message(False)
+          print_message(False, pr_id, user, head_ref, base_ref)
         else:
-          print_message(True)
+          print_message(True, pr_id, user, head_ref, base_ref)
           merge(repo, pr_id)
